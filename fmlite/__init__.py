7b4bda7fc040a1e6d997ede48d7a1467b7c33c47
moriaki3193 <puppis1221@gmail.com>
2018-02-11 23:29:50 +0900

    modify __init__.py

---
+++ fmlite/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#     _______   _      _
+#     /        / \    / \
+#    /______  /   \  /   \
+#   /        /     \/     \
+#  /        /              \
+
+"""
+FMLite - Pure Python Implementation of Factorization Machine.
+"""
+
+from .core import FMLite
