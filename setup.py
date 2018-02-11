590880c9b236f19ca4e2a8b19c8c30620abd7c80
moriaki3193 <puppis1221@gmail.com>
2018-02-11 21:34:16 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='FMLite',
+    version='0.0.1',
+    description='Pure Python Implementation of Factoriazatin Machine.',
+    author='Moriaki Saigusa',
+    author_email='moriaki3193@gmail.com',
+    url='https://github.com/moriaki3193/FMLite',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite='tests'
+)
