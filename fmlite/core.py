3378bd51f081e889d5872620c76924f2d81c2839
moriaki3193 <puppis1221@gmail.com>
2018-02-11 21:33:36 +0900

    modify モデルの修正

--- fmlite/core.py
+++ fmlite/core.py
@@ -32,9 +32,11 @@ class FMLite:
         self.k = k
         self.mode = mode
         self.verbose = verbose
+        self.learning_rate = 0.001  # TODO 学習率が大きいと残渣が得られない
         self.b = None  # set when self.fit is called
         self.w = None  # set when self.fit is called
         self.V = None  # set when self.fit is called
+        self.errors = np.array([])
 
     def _calc_y_pred(self, x):
         """1つの訓練事例に対する現時点での応答変数の推定値を返す.
@@ -45,7 +47,7 @@ class FMLite:
         """
         interaction_sum = np.array([
             np.dot(self.V[i], self.V[j].T) * x[i] * x[j]
-            for i, j in combinations(x.shape[0], 2)
+            for i, j in combinations(range(x.shape[0]), 2)
         ]).sum()
         return self.b + np.dot(self.w, x.T) + interaction_sum
 
@@ -61,28 +63,34 @@ class FMLite:
 
         return
         ------
-        squared_errors : np.array, shape = (n_samples * n_epochs, )
+        errors : np.array, shape = (n_samples * n_epochs, )
         """
         # [START Vの(i, f)成分でy_predを偏微分した値を返す関数の定義]
         # TODO リファクタリング
         def _partial_V(x):
+            """i番目の事例について
+            """
+            n_features = x.shape[0]
             _V = np.empty((n_features, self.k))
-            for f in range(self.k):
-                first_term = np.dot(x, np.asarray(self.V[:, f]).ravel().T)
-                second_term = self.V[:, f] * (x ** 2)
-                _v = first_term + second_term
-                _V[:, f] = _v
-            return _V
+            for i in range(n_features):
+                for f in range(self.k):
+                    s = 0.0
+                    for j in range(n_features):
+                        s += self.V[j, f] * x[j]
+                    _V[i, f] = x[i] * s - self.V[i, f] * x[i] ** 2
+            return np.asmatrix(_V)
+
         # [END Vの(i, f)成分でy_predを偏微分した値を返す関数の定義]
 
         # [START MLSに対する偏微分の値を返す関数の定義]
-        def _evaluate_grad(x, diff):
+        def _evaluate_grad(x_i, diff):
             b_grad = 2 * diff
-            w_grad = 2 * diff * x  # using bloadcasting
-            V_grad = 2 * diff * _partial_V(x)  # using bloadcasting
+            w_grad = 2 * diff * x_i  # using bloadcasting
+            V_grad = 2 * diff * _partial_V(x_i)  # using bloadcasting
             return (b_grad, w_grad, V_grad)
         # [END MLSに対する偏微分の値を返す関数の定義]
-        squared_errors = []
+
+        errors = []
         n_epochs = 100  # エポック数
         n_samples, n_features = X_train.shape
         normal_params = {'scale': 0.01, 'size': (n_features, self.k)}
@@ -96,7 +104,7 @@ class FMLite:
                 y = y_train[ind]
                 y_tmp_pred = self._calc_y_pred(x)
                 diff = y_tmp_pred - y
-                squared_errors.append(diff ** 2)
+                errors.append(diff)
                 b_grad, w_grad, V_grad = _evaluate_grad(x, diff)
                 # [START updating parameters]
                 self.b = self.b - self.learning_rate * b_grad
@@ -104,20 +112,22 @@ class FMLite:
                 self.V = self.V - self.learning_rate * V_grad
                 # [END updating parameters]
         # [END parameter estimation]
-        return np.asarray(squared_errors)
+        return np.asarray(errors)
 
     def fit(self, X_train, y_train):
         """Fitting using SGD methods.
         """
+        if isinstance(y_train, list):
+            y_train = np.asarray(y_train)
         if X_train.ndim != 2:
             raise ValueError('ndim of X must be 2.')
         if X_train.shape[0] != y_train.shape[0]:
             raise ValueError('n_samples of X and y must be equal.')
-        squared_errors = self._gradient_descent(X_train, y_train)  # fitting
+        self.errors = self._gradient_descent(X_train, y_train)  # fitting
         if self.verbose:
             # squared_errors を利用した学習過程の表示
-            print(squared_errors)
+            print(self)
         return self
 
     def predict(self, X):
-        return np.array([self._calc_y_pred(x) for x in X])
+        return np.asarray([self._calc_y_pred(x) for x in X]).flatten()
